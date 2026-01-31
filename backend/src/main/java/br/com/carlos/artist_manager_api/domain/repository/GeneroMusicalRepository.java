33cd30195eb4a87ed42756a99c83d6d36882431d
Carlos Rafael <carlosrafaelnogueiraas@gmail.com>
2026-01-30 20:37:34 -0400

    feat: implementa camada de repositório e consultas dinâmicas
    - Criação da classe AlbumSpecs: Implementa a interface Specification do Spring Data JPA para permitir filtros dinâmicos na listagem de álbuns. Utiliza Criteria API para filtrar por título parcial e realiza um JOIN para filtrar pelo nome do artista associado.
    
    - Artista: Adicionado findByNomeContainingIgnoreCase para busca textual paginada.
    
    - GeneroMusical: Adicionado existsBySlug para validação de unicidade antes da persistência.

--- backend/src/main/java/br/com/carlos/artist_manager_api/domain/repository/GeneroMusicalRepository.java
+++ backend/src/main/java/br/com/carlos/artist_manager_api/domain/repository/GeneroMusicalRepository.java
@@ -4,10 +4,10 @@ import br.com.carlos.artist_manager_api.domain.entity.GeneroMusical;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 import java.util.UUID;
 
 @Repository
 public interface GeneroMusicalRepository extends JpaRepository<GeneroMusical, UUID> {
+    boolean existsBySlug(String slug);
 
 }
