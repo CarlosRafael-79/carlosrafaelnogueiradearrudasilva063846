7d0dbd0a868c66134d95d61698bb222f67b59a67
Carlos Rafael <carlosrafaelnogueiraas@gmail.com>
2026-02-03 22:21:41 -0400

    config: define configuracoes de documentacao OpenAPI e seguranca
    - Inclui a biblioteca springdoc-openapi-starter-webmvc-ui no pom.xml para habilitar a geração automática da documentação da API e disponibilizar a interface do Swagger UI.
    
    - Cria a classe OpenApiConfig para personalizar a interface do Swagger UI, definindo metadados do projeto (título, versão, contato) e registrando o esquema de segurança Bearer JWT para uso futuro.
    
    - Implementa a classe SecurityConfig para desativar a proteção CSRF (padrão para APIs REST stateless) e configurar a cadeia de filtros de segurança permitindo acesso irrestrito a todos os endpoints para fins de desenvolvimento.

---
+++ backend/src/main/java/br/com/carlos/artist_manager_api/infrastructure/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package br.com.carlos.artist_manager_api.infrastructure.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Artist Manager API")
+                        .description("API para gerenciamento de artistas, álbuns e discografia. Desenvolvido para teste Sênior.")
+                        .version("v1")
+                        .contact(new Contact()
+                                .name("Carlos Rafael")
+                                .email("carlos@exemplo.com")));
+    }
+}
